4232f7353ad12ff0005dfd5a202530a403080895
Life75 <austynwashington@yahoo.com>
2021-06-24 11:08:21 -0500

    delete operation in hash complete, need to do for database next as well

--- fullstack/src/App.js
+++ fullstack/src/App.js
@@ -15,6 +15,7 @@ class Customer {
     this.state = '';
     this.zip = '';
     this.businessType = '';
+    this.key = null;
   }
 
   setFirstName(firstname) {this.firstname = firstname;}
@@ -44,6 +45,9 @@ class Customer {
   setBusinessType(businessType) {this.businessType = businessType;}
   getBusinessType() {return this.businessType;}
 
+  setKey(key) {this.key = key;}
+  getKey() {return this.key;}
+
   
 }
 
@@ -60,10 +64,57 @@ class App extends Component {
     this.createCustomers = this.createCustomers.bind(this);
     this.placeArrIntoHash = this.placeArrIntoHash.bind(this);
   //  this.placeIntoHash = this.placeIntoHash.bind(this);
-    this.getHashKey = this.getHashKey.bind(this);
-    this.renderCustomers = this.renderCustomers.bind(this);
+    this.getNewHashKey = this.getNewHashKey.bind(this);
+    this.onDismiss = this.onDelete.bind(this);
+    this.updateList = this.updateList.bind(this);
+  
 
   }
+
+  onDelete(customer) {
+    //TODO find and delete from hash and database 
+    //console.log(customer.getFirstName())
+    //var holder = [...this.state.customerArr];
+    this.deleteFromHash(customer.getKey())
+    //console.log(key);
+    //this.setState({customerArr[this.getHashKey(customer,this.customerArr.length)] : null})
+  }
+
+  deleteFromHash(key) {
+    var holder = [...this.state.customerArr] //needs to be copied over because data is immutable when dealing w/ react 
+    holder[key] = null;
+    this.setState({customerArr: holder});
+    console.log('deleted customer key: ' + key);
+    //this.updateList();
+
+  }
+
+  updateList() {
+
+      this.state.customerArr.map((customer) => 
+      <details key={customer.getFirstName()}>
+        <summary>
+          {customer.getFirstName()} {customer.getLastName()}
+        </summary>
+          Name: {customer.getFirstName()}&nbsp;{customer.getLastName()}<br/>
+          Description: {customer.getDescription()}<br/>
+          Address Line 1: {customer.getAddressOne()}<br/>
+          Address Line 2: {customer.getAddressTwo()}<br/>
+          City: {customer.getCity()}<br/>
+          State: {customer.getState()}<br/>
+          Zip: {customer.getZip()}<br/>
+          Business Type: {customer.getBusinessType()}<br/>
+          <Button 
+          onClick={() => this.onDelete(customer)}
+  
+          >
+            Delete
+          </Button>
+      </details> 
+    )
+    
+  }
+
   //TODO add adding/removing into hash and db functionality
   placeArrIntoHash(customerHolder, maxSize) {
     maxSize += 100;
@@ -71,11 +122,12 @@ class App extends Component {
 
     while(customerHolder.length != 0) {
       var customer = customerHolder.pop();
-      var key = this.getHashKey(customer, maxSize);
+      var key = this.getNewHashKey(customer, maxSize);
 
       console.log('length: ' + (customer.getFirstName().length + customer.getLastName().length));
       console.log('key: ' + key);
-
+      
+      customer.setKey(key);
       this.customerArr[key]=customer;
       console.log(this.customerArr[key].getFirstName());
       this.setState({customerArr : this.customerArr})
@@ -83,14 +135,18 @@ class App extends Component {
 
   }
 
-
-  getHashKey(customer, maxSize) {
+//finds an open free hash
+//TODO fault with how set up check over later for scalability concerns, works for now
+  getNewHashKey(customer, maxSize) {
    var key = maxSize % (customer.getFirstName().length + customer.getLastName().length);
    while(this.state.customerArr[key] != null) {
      key++;
    }
    return key;
   }
+
+//finds the hash in an O(1) search 
+
 
 /*
   placeIntoHash(customer, maxSize) {
@@ -145,38 +201,68 @@ class App extends Component {
 
   componentDidMount() {
     this.getCustomers();
+    
   }
 
   getCustomers = _ => {
-    return fetch('http://localhost:3001')
+    return fetch('http://localhost:3001')//TODO format into vars
     .then(result => result.json())
     .then(data => {
         this.sqlParser(data);
         console.log(this.state.customerArr.length)
-        this.renderCustomers();
     })
   }
 
-  renderCustomers () {
-    this.state.customerArr.map((customer) => <p>{customer.getFirstName()}</p>)
-    
-    console.log()
-    
-   
-   // console.log("here");
-  }
+
+
+
+
 
  // renderCustomers = 
   render() {
-    const customers = this.state.customerArr.map((c) => <details key={c.getFirstName()}><summary>{c.getFirstName()}</summary>{c.getZip()}</details>)
+    //TODO put into a func call later 
+
+    var renderCustomers =
+      this.state.customerArr.map((customer) => customer ? 
+      <details key={customer.getFirstName()}>
+        <summary>
+          {customer.getFirstName()} {customer.getLastName()}
+        </summary>
+          Name: {customer.getFirstName()}&nbsp;{customer.getLastName()}<br/>
+          Description: {customer.getDescription()}<br/>
+          Address Line 1: {customer.getAddressOne()}<br/>
+          Address Line 2: {customer.getAddressTwo()}<br/>
+          City: {customer.getCity()}<br/>
+          State: {customer.getState()}<br/>
+          Zip: {customer.getZip()}<br/>
+          Business Type: {customer.getBusinessType()}<br/>
+          <Button 
+          onClick={() => this.onDelete(customer)}
+  
+          >
+            Delete
+          </Button>
+      </details> 
+      : null
+    )
+ 
+
     return (
       <div className="App">
-       {customers}
+       {renderCustomers}
       </div>
     );
   }
 }
 
+const Button = ({onClick, className='defaultButton', children}) =>
+  <button
+    onClick={onClick}
+    className={className}
+    type="button"
+    >
+      {children}
+    </button>
 
 
 
